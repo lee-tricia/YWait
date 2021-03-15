462b9450ec6b30fde83baaa58a997414f4213aeb
Hazel <e0420648@u.nus.edu>
2021-03-16 02:46:29 +0800

    Added Firebase authentication

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,21 +1,113 @@
 <template>
-    
+  <div id="Signup">
+    <p>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        v-model="name"
+        placeholder="Name"
+      />
+    </p>
+    <p>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        v-model="email"
+        placeholder="Email Address"
+      />
+    </p>
+    <p>
+      <input
+        type="tel"
+        name="contact"
+        id="contact"
+        v-model="contact"
+        placeholder="Contact Number"
+      />
+    </p>
+    <p>
+      <input
+        type="date"
+        name="dob"
+        id="dob"
+        v-model="dob"
+        placeholder="Date of Birth"
+      />
+      {{ this.dob }}
+    </p>
+    <p>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        v-model="password"
+        placeholder="Password"
+      />
+      {{ this.password }}
+    </p>
+    <p>
+      <input
+        type="password"
+        name="confirmPassword"
+        id="confirmPassword"
+        v-model="confirmPassword"
+        placeholder="Confirm Password"
+      />
+      {{ this.confirmPassword }}
+    </p>
+    <button @click="signup()">Sign Up Now</button>
+  </div>
 </template>
 
 <script>
+//import { database } from "../firebase.js";
+import { auth } from "../firebase.js";
 export default {
-    data() {
-        return {
-
-        }
+  data() {
+    return {
+      name: "",
+      email: "",
+      contact: null,
+      dob: null,
+      password: "",
+      confirmPassword: "",
+    };
+  },
+  methods: {
+    signup() {
+      auth
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          if (this.password != this.confirmPassword) {
+            throw new Error();
+          } else {
+            alert("Successfully registered! Please login.");
+          }
+        })
+        .catch((error) => {
+          if (error.code == "auth/invalid-email") {
+            alert(
+              "The email you entered is invalid. Please check your email and try again."
+            );
+          } else if (error.code == "auth/email-already-in-use") {
+            alert(
+              "Email already exists. Please enter a different email and try again."
+            );
+          } else if (this.password.length < 6) {
+            alert(
+              "Password must contain at least 6 characters. Please check your password and try again."
+            );
+          } else if (this.password != this.confirmPassword) {
+            alert(
+              "Passwords do not match. Please check your password and try again."
+            );
+          }
+        });
     },
-    methods: {
-        
-    }
-}
+  },
+};
 </script>
 
-
-<style>
-
-</style>
+<style></style>
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,18 @@
-import firebase from "firebase"
+import firebase from 'firebase/app';
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJ93UPVuVoF4o06VlutkA5YZXW5u0FAHc",
-    authDomain: "ywait-2eeec.firebaseapp.com",
-    projectId: "ywait-2eeec",
-    storageBucket: "ywait-2eeec.appspot.com",
-    messagingSenderId: "539277049460",
-    appId: "1:539277049460:web:8ba1871ffb550eead31b21",
-    measurementId: "G-L0RP3Z74N0"
-  };
+  apiKey: "AIzaSyCJ93UPVuVoF4o06VlutkA5YZXW5u0FAHc",
+  authDomain: "ywait-2eeec.firebaseapp.com",
+  projectId: "ywait-2eeec",
+  storageBucket: "ywait-2eeec.appspot.com",
+  messagingSenderId: "539277049460",
+  appId: "1:539277049460:web:8ba1871ffb550eead31b21",
+  measurementId: "G-L0RP3Z74N0"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-var database = firebase.firestore();
-export default database;
+
+export const database = firebase.firestore();
+export const auth = firebase.auth();
