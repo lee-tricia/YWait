00a846a07315f80df2fec9206f00e0561eea504e
Rouxuanpipi <33775419+Rouxuanpipi@users.noreply.github.com>
2021-03-18 13:42:20 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Signup from "./components/Signup.vue"
-
 import NavBar from "./components/NavBar.vue"
-import Malls from "./components/Malls.vue"
-import MyProfile from './components/MyProfile.vue'
-import JoinQ from './components/JoinQ.vue'
-import WaitingQueue from './components/WaitingQueue.vue'
+// import Malls from "./components/Malls.vue"
+// import MyProfile from './components/MyProfile.vue'
+// import JoinQ from './components/JoinQ.vue'
+// import WaitingQueue from './components/WaitingQueue.vue'
 
 import VueRouter from 'vue-router'
 import Routes from './routes.js'
@@ -14,10 +13,10 @@ import Routes from './routes.js'
 Vue.config.productionTip = false
 Vue.component('NavBar', NavBar)
 Vue.component('Signup', Signup)
-Vue.component('Malls', Malls)
-Vue.component('JoinQ', JoinQ)
-Vue.component('MyProfile', MyProfile)
-Vue.component('WaitingQueue', WaitingQueue)
+// Vue.component('Malls', Malls)
+// Vue.component('JoinQ', JoinQ)
+// Vue.component('MyProfile', MyProfile)
+// Vue.component('WaitingQueue', WaitingQueue)
 Vue.use(VueRouter)
 
 const myRouter =  new VueRouter({
