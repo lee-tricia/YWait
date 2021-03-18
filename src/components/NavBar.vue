7c331b65ffa2fb3e315c23c54bcb3a9a160405bd
Rouxuanpipi <33775419+Rouxuanpipi@users.noreply.github.com>
2021-03-18 13:17:28 +0800

    Update NavBar Style

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,20 +2,27 @@
   <div>
     <nav>
       <ul>
-        <li><router-link to="/" exact>Home</router-link></li>
-        <li><router-link to="/waitingqueue" exact>Waiting Queues</router-link></li>
-        <li><router-link to="/malls" exact>Malls</router-link></li>
-        <li><router-link to="/joinq" exact>Join a Queue</router-link></li>
-        <li><router-link to="/myprofile" exact>My Profile</router-link></li>
+        <li><router-link to="/" exact>Home</router-link></li><br>
+        <li><router-link to="/waitingqueue" exact>Waiting Queues</router-link></li><br>
+        <li><router-link to="/malls" exact>Malls</router-link></li><br>
+        <li><router-link to="/joinq" exact>Join a Queue</router-link></li><br>
+        <li><router-link to="/myprofile" exact>My Profile</router-link></li><br>
       </ul>
     </nav>
   </div>
 </template>
 
 <style scoped>
-ul{
-    list-style-type: none;
+ul {
+  list-style-type: none;
 }
 
-
+nav {
+  float: left;
+  width: 30%;
+  background: #e9e9e2;
+  padding-right: 20px;
+  height: 300px;
+  font-size: 20px;
+}
 </style>
