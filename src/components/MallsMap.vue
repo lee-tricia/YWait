3e69e0908f15b7243e7b02c35dcec0677ca9f993
jiexilim <jiexilim.ljx@gmail.com>
2021-03-21 23:09:12 +0800

    Added MallsMap.vue
    Added map feature in Malls page

--- src/components/MallsMap.vue
+++ src/components/MallsMap.vue
@@ -1,16 +1,13 @@
 <template>
 <div class="hello">
-  <h2>Map</h2>
+  <h1>{{ msg }}</h1>
   <div class="card">
+    <h3>Map</h3>
       <div class="card-body">
         <kendo-map :center="[1.3051315545985274, 103.83224131197983]"          :zoom="13" style="width: 100%; height: 500px;">
-        <kendo-map-layer :type="'tile'"
-                         :url-template="'https://#= subdomain #.tile.openstreetmap.org/#= zoom #/#= x #/#= y #.png'"
-                         :subdomains="['a', 'b', 'c']"
-                         :attribution="'&copy; <a href=\'https://osm.org/copyright\'>OpenStreetMap contributors</a>'">
-        </kendo-map-layer>
-
-        <kendo-map-marker v-for="mall in mallsList" :location="mall.coor" v-bind: key="mall.id">        </kendo-map-marker>
+        <kendo-map-layer          :type="'tile'"          :url-template="'https://#= subdomain #.tile.openstreetmap.org/#= zoom #/#= x #/#= y #.png'"
+          :subdomains="['a', 'b', 'c']"          :attribution="'&copy; <a href=\'https://osm.org/copyright\'> OpenStreetMap contributors</a>'">        </kendo-map-layer>
+        <kendo-map-marker           :location="[1.3042291286530805, 103.8319619254719]">        </kendo-map-marker>
 
         <kendo-map-marker           :location="[1.3347372693919985, 103.74265061197976]">        </kendo-map-marker>
 
@@ -19,7 +16,6 @@
         <kendo-map-marker           :location="[1.2991516889234715, 103.85561997301599]">        </kendo-map-marker>
 
         <kendo-map-marker           :location="[1.2952635060359494, 103.85827269134388]">        </kendo-map-marker>
-      
       </kendo-map>
     </div>
   </div>
@@ -27,43 +23,17 @@
 </template>
 
 <script>
-
-import { database } from "../firebase.js";
-
-    export default {
-        name: "Map",
-
-        data() {
-            return {
-                mallsList: []
-            }
-        },
-
-        methods: {
-
-            fetchItems: function() {
-                database.collection('malls').get().then((querySnapShot) => {
-                    let mall = {};
-                    querySnapShot.forEach(doc => {
-                        let coor = doc.data().coordinate;
-                        mall.coor = [coor.latitude, coor.longitude];
-                        console.log(mall.coor)
-                        mall.id = doc.id;
-                        this.mallsList.push(mall);
-                    })
-                })
-        }
-    },
-
-        created() {
-            this.fetchItems()
-        }
-    };
+export default {
+  name: "Map",
+  props: {
+    msg: String
+  }
+};
 </script>
 
 <style scoped>
+
   .card-body {
   width: 500px;
   }
-
 </style>
