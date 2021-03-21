e955ca7582bad911cb40d20f8515c65b44b6c82e
lee-tricia <80332823+lee-tricia@users.noreply.github.com>
2021-03-21 16:23:05 +0800

    Added restaurantView JS

---
+++ src/components/restaurantView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+
+    {{ currentQueueNumber }}
+    <ul>
+      <li v-for="queue in currentQueueNumber" v-bind:key="queue.id">
+        {{queue}} <br>
+        <button v-bind:id="queue['bookingID']" v-on:click="changeStatus($event, 'done')">Update</button>
+      </li>
+    </ul>
+    
+    {{ waitingQueue }}
+    <ul>
+      <li v-for="queue in waitingQueue" v-bind:key="queue.id">
+        {{queue}} <br>
+        <button v-bind:id="queue['bookingID']" v-on:click="changeStatus($event, 'current')">Push</button>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import { database } from "../firebase.js"
+import { auth } from "../firebase.js"
+
+export default {
+  data() {
+    return {
+      restaurantName: "",
+      currentQueueNumber: this.queueData('current'),
+      waitingQueue: this.queueData('waiting')
+    }
+  },
+
+  methods: {
+    getDataFromQuery: function(query) {
+      var data = []
+      query.get().then(snapshot => {
+        snapshot.docs.forEach(doc => {
+          data.push(doc.data())
+        })
+      })
+      return data
+    },
+    queueData: function(status) {
+      var query = database.collection('Bookings').where('restaurantName','==',this.restaurantName).where('queueStatus','==',status)
+      return this.getDataFromQuery(query)
+    },
+    changeStatus: function(event, newStatus) {
+      let doc_id = event.target.id
+      database.collection('Bookings').doc(doc_id).update({
+        queueStatus: newStatus
+      }).then(() => location.reload())
+    },
+    getRestaurantName: function() {
+      database.collection('restaurant').doc(`${auth.currentUser.uid}`).get().then(doc => {
+        this.restaurantName = doc.data().restaurantName
+      })
+    }
+  },
+  created() {
+    this.getRestaurantName()
+  }
+}
+</script>
+
+<style>
+
+</style>
