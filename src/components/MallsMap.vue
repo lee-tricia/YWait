4baf25ae9b2b04c228098b16d04358443123254b
jiexilim <jiexilim.ljx@gmail.com>
2021-03-31 14:05:40 +0800

    Update Malls.vue
    Updated the map markers

--- src/components/MallsMap.vue
+++ src/components/MallsMap.vue
@@ -6,7 +6,7 @@
       <div class="card-body">
         <kendo-map
           :center="[1.3051315545985274, 103.83224131197983]"
-          :zoom="13"
+          :zoom="12"
           style="width: 100%; height: 500px; margin-left: 35px"
         >
           <kendo-map-layer
@@ -15,25 +15,37 @@
             :subdomains="['a', 'b', 'c']"
             :attribution="'&copy; <a href=\'https://osm.org/copyright\'> OpenStreetMap contributors</a>'"
           >
+
+          // ION Orchard
           </kendo-map-layer>
-          <kendo-map-marker :location="[1.3042291286530805, 103.8319619254719]">
+          <kendo-map-marker :location="[1.3041594102285439, 103.83223014972324]">
           </kendo-map-marker>
 
+          // Jewel Changi Airport
           <kendo-map-marker
-            :location="[1.3347372693919985, 103.74265061197976]"
+            :location="[1.3604823693988464, 103.98973855951958]"
           >
           </kendo-map-marker>
 
-          <kendo-map-marker :location="[1.2646981711296041, 103.822110540476]">
+          // Westgate
+          <kendo-map-marker :location="[1.334732192769414, 103.74266480212091]">
           </kendo-map-marker>
 
+          // VivoCity
           <kendo-map-marker
-            :location="[1.2991516889234715, 103.85561997301599]"
+            :location="[1.2646123613498939, 103.8221963711645]"
           >
           </kendo-map-marker>
 
+          // Bugis Junction
           <kendo-map-marker
-            :location="[1.2952635060359494, 103.85827269134388]"
+            :location="[1.299140962845494, 103.85543758280939]"
+          >
+          </kendo-map-marker>
+
+          // Suntec City
+          <kendo-map-marker
+            :location="[1.295360041820001, 103.85918464574937]"
           >
           </kendo-map-marker>
         </kendo-map>
