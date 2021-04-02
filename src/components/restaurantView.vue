b82a5ce586d2cf65098b2cab4aac1f37e1ba3ff4
lee-tricia <80332823+lee-tricia@users.noreply.github.com>
2021-04-03 03:13:04 +0800

    Updated RestaurantView html

--- src/components/restaurantView.vue
+++ src/components/restaurantView.vue
@@ -1,29 +1,70 @@
 <template>
   <div>
-    <h1>Restaurant page</h1>
-    <ul>
-      <li v-for="queue in currentQueueNumber" v-bind:key="queue.id">
-        {{ queue }} <br />
-        <button
-          v-bind:id="queue['bookingID']"
-          v-on:click="changeStatus($event, 'completed')"
-        >
-          Update
-        </button>
-      </li>
-    </ul>
+    <h1>Restaurant Queue System</h1>
+    <div id="currToCompleted">
+      <table>
+        <tr>
+          <th>No. Pax</th>
+          <th>Current Queue Number</th>
+          <th>Customer ID</th>
+          <th>Baby Chair</th>
+          <th>Wheel Chair</th>
+          <th>Additional Message</th>
+          <th>Button</th>
+        </tr>
 
-    <ul>
-      <li v-for="queue in waitingQueue" v-bind:key="queue.id">
-        {{ queue }} <br />
-        <button
-          v-bind:id="queue['bookingID']"
-          v-on:click="changeStatus($event, 'current')"
-        >
-          Push
-        </button>
-      </li>
-    </ul>
+        <tr v-for="queue in currentQueueNumber" v-bind:key="queue.id">
+          <td> {{queue['paxGroup']}} </td>
+          <td> {{queue['queueNumber']}} </td>
+          <td> {{queue['customerID']}} </td>
+          <td> {{queue['babyChair']}} </td>
+          <td> {{queue['wheelChair']}} </td>
+          <td> {{queue['additionalMessage']}} </td>
+          <td> 
+            <button
+            v-bind:id="queue['bookingID']"
+            v-on:click="changeStatus($event, 'completed')"
+          >
+            Update
+          </button>            
+          </td>          
+        </tr>
+      </table>
+    </div>
+
+    <div id="waitingToCurr">
+      <table>
+        <tr>
+          <th>No. Pax</th>
+          <th>Queue Number</th>
+          <th>Customer ID</th>
+          <th>Baby Chair</th>
+          <th>Wheel Chair</th>
+          <th>Additional Message</th>
+          <th>Arrival Time</th>
+          <th>Button</th>
+        </tr>
+
+        <tr v-for="queue in waitingQueue" v-bind:key="queue.id">
+          <td> {{queue['paxGroup']}} </td>
+          <td> {{queue['queueNumber']}} </td>
+          <td> {{queue['customerID']}} </td>
+          <td> {{queue['babyChair']}} </td>
+          <td> {{queue['wheelChair']}} </td>
+          <td> {{queue['additionalMessage']}} </td>
+          <td> {{queue['arrivalTime']}} </td>
+          <td> 
+            <button
+            v-bind:id="queue['bookingID']"
+            v-on:click="changeStatus($event, 'current')"
+          >
+            Push
+          </button>          
+          </td>          
+        </tr>
+      </table>
+    </div>
+
   </div>
 </template>
 
